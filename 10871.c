8db8c262bdba4c0791c0c7a51cc86aa9f7a3c562
JediMasterJune <anbnd5@gmail.com>
2021-06-26 17:11:49 +0900

    first commit

---
+++ 10871.c
@@ -0,0 +1,27 @@
+//
+// Created by JediMasterJune on 21. 2. 2..
+//
+
+#include <stdio.h>
+#define LENGTH 100000
+
+int main(void)
+{
+    int n, x;
+    scanf("%d %d",&n,&x);
+    int array[LENGTH] = {0};
+    for (int i = 0; i<n; i++)
+    {
+        int mario;
+        scanf("%d",&mario);
+        array[i]=mario;
+    }
+    for (int i = 0; i<n; i++)
+    {
+        if (array[i]<x)
+        {
+            printf("%d ",array[i]);
+        }
+    }
+return 0;
+}
