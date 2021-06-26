aaeae7199240d3d9f1d96d632b15f8fe00222649
JediMasterJune <anbnd5@gmail.com>
2021-06-26 21:34:44 +0900

    Added zeros for sorting

--- 10871.c
+++ 10871.c
@@ -1,27 +1,25 @@
 //
-// Created by JediMasterJune on 21. 2. 2..
+// Created by JediMasterJune on 21. 02. 02..
 //
 
 #include <stdio.h>
+
 #define LENGTH 100000
 
 int main(void)
 {
     int n, x;
-    scanf("%d %d",&n,&x);
+    scanf("%d %d", &n, &x);
     int array[LENGTH] = {0};
-    for (int i = 0; i<n; i++)
-    {
+    for (int i = 0; i < n; i++) {
         int mario;
-        scanf("%d",&mario);
-        array[i]=mario;
+        scanf("%d", &mario);
+        array[i] = mario;
     }
-    for (int i = 0; i<n; i++)
-    {
-        if (array[i]<x)
-        {
-            printf("%d ",array[i]);
+    for (int i = 0; i < n; i++) {
+        if (array[i] < x) {
+            printf("%d ", array[i]);
         }
     }
-return 0;
+    return 0;
 }
